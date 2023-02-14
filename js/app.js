4890e5fa12ea1ec6c84d887df3257bf4cf515b05
Cody Lobdell <thecreator85@gmail.com>
2023-02-14 09:32:01 -0800

    added initial Navigation function

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+tableHeader();
+renderAll();
+tableFooter();
