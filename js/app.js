46825743d584d8f391751be23c0b33ba6c066a25
Cody Lobdell <thecreator85@gmail.com>
2023-02-14 18:12:20 -0800

    added backround rocks to HTML and JS funcyionality for clicks

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 'use strict';
 
+// Functionality for clicks
+
+$('#home').click(function () {
+  window.location.href = 'index.html'
+})
+
+$('#profiles').click(function () {
+  window.location.href = '---.html'
+})
+
+$('#aboutUs').click(function () {
+  window.location.href = '---.html'
+})
+
+$('#PLACEHOLDER').click(function () {
+  window.location.href = '---.html'
+})
+
+
+$('.placeholder').click(function () {
+  $('.placeholder p').show();
+  new Info('placeholder');
+  total += 9
+  localStorage.setItem('profileInfo', JSON.stringify(formData));
+  setTimeout(function () {
+      $('.placeholder p').hide()
+  }, 1500);
+})
