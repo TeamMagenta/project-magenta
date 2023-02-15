5af0793b95ceba38af29ec73d14ac12eb4cd06a4
Cody Lobdell <thecreator85@gmail.com>
2023-02-15 14:12:58 -0800

    edited nav bar and tried to get form page working with L/D toggle.

--- js/form.js
+++ js/form.js
@@ -1,8 +1,46 @@
-function myFunction(t); {
-  if ($(t).prev().val()) {
-    alert('Adding Succesful!');
-    return true;
+
+
+/*Light Dark Toggle*/
+
+const btn = document.querySelector('button')
+
+//FUNCTIONS
+function soundClick() {
+  let audio = new Audio('click-one.mp3');
+  audio.play();
+}
+function soundClickTwo() {
+  let audio = new Audio("click-two.mp3");
+  audio.play();
+}
+const darkMode = () => {
+  const body = document.querySelector('body');
+  const sun = document.querySelector('i');
+  const mode = document.querySelector('h1');
+  body.classList.toggle('darkMode');
+
+  if (body.classList.contains('darkMode')) {
+    sun.className = 'fas fa-moon fa-3x';
+    mode.textContent = "Dark Mode";
+    sun.classList.add('open');
+    soundClick();
+    localStorage.setItem('theme', 'dark');
   } else {
-    return false;
+    sun.className = 'fas fa-sun fa-3x';
+    mode.textContent = "Light Mode";
+    sun.classList.add('close');
+    soundClickTwo();
+    localStorage.setItem('theme', 'light');
   }
+};
+
+//EVENT LISTENER
+btn.addEventListener('click', darkMode);
+
+//LOCAL STORAGE
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+  darkMode();
+} else {
+  localStorage.removeItem('dark');
 }
