8096bbf5b1e7ba3b1a915d816dddd66ec2d26150
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-02-16 19:15:56 -0800

    homepage and about us working

--- js/lightdarktheme.js
+++ js/lightdarktheme.js
@@ -4,17 +4,16 @@ const btn = document.querySelector('button');
 const darkMode = () => {
   const body = document.querySelector('body');
   const sun = document.querySelector('i');
-  const mode = document.querySelector('h1');
   body.classList.toggle('darkMode');
 
   if(body.classList.contains('darkMode')) {
     sun.className = 'fas fa-moon fa-3x';
-    mode.textContent = 'Dark Mode';
+    // mode.textContent = 'Dark Mode';
     sun.classList.add('open');
     localStorage.setItem('theme', 'dark');
   } else {
     sun.className = 'fas fa-sun fa-3x';
-    mode.textContent = 'Light Mode';
+    // mode.textContent = 'Light Mode';
     sun.classList.add('close');
     localStorage.setItem('theme', 'light');
   }
